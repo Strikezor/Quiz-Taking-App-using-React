6f1b725c19a928f3e850904d223ac2bddc0f4e8d
Strikezor <akshaychaturvedi12345@gmail.com>
2024-07-10 16:51:09 +0530

    first Commit

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    question: "What is the capital of India?",
+    options: ["Mumbai", "Pune", "Nagpur", "Delhi"],
+    answer: "Delhi",
+    id: 1,
+  },
+  {
+    question: "Who is the Prime Minister of India?",
+    options: ["Rahul Gandhi", "Nitin Gadkari", "Narendra Modi", "Sharad Pawar"],
+    answer: "Narendra Modi",
+    id: 2,
+  },
+
+  {
+    question: "when India got independence?",
+    options: ["1950", "1947", "1930", "1945"],
+    answer: "1947",
+    id: 3,
+  },
+  {
+    question: "How many planets are there in our solar system?",
+    options: ["7", "8", "9", "10"],
+    answer: "8",
+    id: 4,
+  },
+];
+export default questions;
